bfcaf35ca5a008da1cbdf22e71fcd95743fc78ea
Kevin Middleton <kevin.middleton@gmail.com>
2024-01-06 16:12:38 -0500

    Add files via upload

--- photopopup/PhotoPopup.js
+++ photopopup/PhotoPopup.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    console.log("Window loaded and script running");
     var modal = document.getElementById("myModal");
     var modalImg = document.getElementById("img01");
     var captionText = document.getElementById("caption");
@@ -9,6 +10,7 @@ window.onload = function() {
     var closeButton = document.querySelector('.close');
 
     function openModalImage(index) {
+        console.log("Opening modal for image index: ", index);
         modal.style.display = "block";
         modalImg.src = imageLinks[index].href;
         captionText.innerHTML = imageLinks[index].alt;
@@ -18,20 +20,23 @@ window.onload = function() {
     imageLinks.forEach((item, index) => {
         item.addEventListener('click', event => {
             event.preventDefault();
+            console.log("Image link clicked: ", index);
             openModalImage(index);
         });
     });
 
     if (prevButton) {
         prevButton.addEventListener('click', () => {
+            console.log("Previous button clicked");
             if (currentIndex > 0) {
                 openModalImage(currentIndex - 1);
             }
         });
     }
-
+    
     if (nextButton) {
         nextButton.addEventListener('click', () => {
+            console.log("Next button clicked");
             if (currentIndex < imageLinks.length - 1) {
                 openModalImage(currentIndex + 1);
             }
@@ -40,6 +45,7 @@ window.onload = function() {
 
     if (closeButton) {
         closeButton.addEventListener('click', () => {
+            console.log("Close button clicked");
             modal.style.display = "none";
         });
     }
