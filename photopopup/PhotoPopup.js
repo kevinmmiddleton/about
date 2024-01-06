a8dd44edfc69156564412cb1247cac4df1f1a607
Kevin Middleton <kevin.middleton@gmail.com>
2024-01-06 15:32:57 -0500

    Add files via upload

--- photopopup/PhotoPopup.js
+++ photopopup/PhotoPopup.js
@@ -1,35 +1,46 @@
-var modal = document.getElementById("myModal");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-var imageLinks = document.querySelectorAll('.image-link');
-var currentIndex;
+window.onload = function() {
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    var imageLinks = document.querySelectorAll('.image-link');
+    var currentIndex;
+    var prevButton = document.querySelector('.prev');
+    var nextButton = document.querySelector('.next');
+    var closeButton = document.querySelector('.close');
 
-function openModalImage(index) {
-  modal.style.display = "block";
-  modalImg.src = imageLinks[index].href;
-  captionText.innerHTML = imageLinks[index].alt;
-  currentIndex = index;
-}
+    function openModalImage(index) {
+        modal.style.display = "block";
+        modalImg.src = imageLinks[index].href;
+        captionText.innerHTML = imageLinks[index].alt;
+        currentIndex = index;
+    }
 
-imageLinks.forEach((item, index) => {
-  item.addEventListener('click', event => {
-    event.preventDefault();
-    openModalImage(index);
-  });
-});
+    imageLinks.forEach((item, index) => {
+        item.addEventListener('click', event => {
+            event.preventDefault();
+            openModalImage(index);
+        });
+    });
 
-document.querySelector('.prev').addEventListener('click', () => {
-  if (currentIndex > 0) {
-    openModalImage(currentIndex - 1);
-  }
-});
+    if (prevButton) {
+        prevButton.addEventListener('click', () => {
+            if (currentIndex > 0) {
+                openModalImage(currentIndex - 1);
+            }
+        });
+    }
 
-document.querySelector('.next').addEventListener('click', () => {
-  if (currentIndex < imageLinks.length - 1) {
-    openModalImage(currentIndex + 1);
-  }
-});
+    if (nextButton) {
+        nextButton.addEventListener('click', () => {
+            if (currentIndex < imageLinks.length - 1) {
+                openModalImage(currentIndex + 1);
+            }
+        });
+    }
 
-document.querySelector('.close').addEventListener('click', () => {
-  modal.style.display = "none";
-});
+    if (closeButton) {
+        closeButton.addEventListener('click', () => {
+            modal.style.display = "none";
+        });
+    }
+};
